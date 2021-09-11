fc2fa4e96c1f31b56b3606a1faad17a3d66bdcb7
Vikash UI_GEMS <cvvkshcv@gmail.com>
2021-09-12 02:03:02 +0530

    added express route and cors

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const cors = require('cors')
+const userRoute = require('./routes/user');
+const app = express();
+const PORT = 4444;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Post method'});
+});
+app.use('/user', userRoute);
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
