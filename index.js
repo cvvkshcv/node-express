159ea91e950547962ac1a8bcde8068e0b1e52683
Vikash UI_GEMS <cvvkshcv@gmail.com>
2021-09-22 23:24:06 +0530

    video call enabled

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const express = require('express');
-const cors = require('cors')
-const userRoute = require('./routes/user');
+const express = require("express");
+const http = require("http");
 const app = express();
-const PORT = 4444;
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-app.use(cors());
+const rooms = {};
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Post method'});
+io.on("connection", socket => {
+  socket.on("join room", roomID => {
+    if (rooms[roomID]) {
+      rooms[roomID].push(socket.id);
+    } else {
+      rooms[roomID] = [socket.id];
+    }
+    const otherUser = rooms[roomID].find(id => id !== socket.id);
+    if (otherUser) {
+      socket.emit("other user", otherUser);
+      socket.to(otherUser).emit("user joined", socket.id);
+    }
+  });
+
+  socket.on("offer", payload => {
+    io.to(payload.target).emit("offer", payload);
+  });
+
+  socket.on("answer", payload => {
+    io.to(payload.target).emit("answer", payload);
+  });
+
+  socket.on("ice-candidate", incoming => {
+    io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+  });
+
+  socket.on("disconnect", _ => {
+    console.log("disconnected", socket.id);
+  })
 });
-app.use('/user', userRoute);
 
-app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`);
-});
+
+server.listen(8000, () => console.log('server is running on port 8000'));
